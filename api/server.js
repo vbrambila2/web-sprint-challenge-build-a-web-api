11038a4590248f56b18d5e0c02e27d3d4cc4613f
Victor Brambila <victorbrambila22@gmail.com>
2022-04-14 15:43:08 -0700

    initial setup

--- api/server.js
+++ api/server.js
@@ -5,5 +5,12 @@ const server = express();
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json());
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
 
 module.exports = server;
